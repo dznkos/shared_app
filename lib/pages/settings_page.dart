38f729d32b07fd3860e480b58ac00732f1cb3a67
Luis Vice <khanccer@gmail.com>
2020-09-22 20:34:05 -0500

    ver_0.2

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:shared_app/widgets/drawer_style.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
 
   static final String routeName = 'settings';
+
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+
+  bool _estadoUsuario = false;
+  int _genero = 1;
+  String _nombre = 'Pedro';
+
+  TextEditingController _textController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _textController = new TextEditingController( text: _nombre);
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -12,9 +32,55 @@ class SettingsPage extends StatelessWidget {
         title: Text('Ajustes'),
       ),
       drawer: MenuDrawer(),
-      body: Center(
-        child: Text('Ajustes Page'),
-      )
+      body: ListView(
+        children: [
+          Container(
+            padding: EdgeInsets.all(15.0),
+            child: Text('Settings', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),),
+          ),
+          SwitchListTile(
+            value: _estadoUsuario ,
+            title: Text('Modo Invisible'), 
+            onChanged: (value) {
+              setState(() {
+                _estadoUsuario = value;  
+              });
+            }
+          ),
+          RadioListTile(
+            value: 1, 
+            title: Text('Masculino'),
+            groupValue: _genero, 
+            onChanged: (value){
+              setState(() {
+                _genero = value;                
+              });
+            }
+          ),
+          RadioListTile(
+            value: 1, 
+            title: Text('Femenino'),
+            groupValue: _genero, 
+            onChanged: (value){
+              setState(() {
+                _genero = value;                
+              });
+            }
+          ),
+          Divider(),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            child: TextField(
+              controller: _textController,
+              decoration: InputDecoration(
+                labelText: 'Nombre',
+                hintText: 'Ingrese nombres'
+              ),
+            )
+          ),
+          
+        ],
+      )      
     );
   }
 }
